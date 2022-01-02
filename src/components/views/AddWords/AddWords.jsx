ea8db34f069e1500210b4f0733564714517eefb4
yukyung123 <rladbrud12@naver.com>
2022-01-03 03:56:35 +0900

    단어추가기능,useRef

--- src/components/views/AddWords/AddWords.jsx
+++ src/components/views/AddWords/AddWords.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { ColorButton, CssTextField } from "../../style/LoginStyle";
 import Input from "@mui/material/Input";
 import NativeSelect from "@mui/material/NativeSelect";
@@ -6,65 +6,52 @@ import styled from "styled-components";
 import AddWordsForm from "./AddWordsForm";
 import axios from "axios";
 import Cookies from "universal-cookie";
-import { Cookie } from "@mui/icons-material";
 
 export default function AddWords() {
-  const [contents, setContents] = useState("");
-  const [spelling, setSpelling] = useState("");
-  const [meaning, setMeaning] = useState("");
-  const [category, setCategory] = useState("n");
   const cookies = new Cookies();
-  const CookieToken = cookies.get("Token");
+  const cookieToken = cookies.get("Token");
 
-  const onClickContentHandler = (event) => {
-    setContents(event.target.value);
-  };
-
-  const onClickSpellingHandler = (event) => {
-    setSpelling(event.target.value);
-  };
-
-  const onClickMeaningHandler = (event) => {
-    setMeaning(event.target.value);
-  };
-
-  const onClickCategoryHandler = (event) => {
-    setCategory(event.target.value);
-  };
+  const contentsRef = useRef(null);
+  const spellingRef = useRef(null);
+  const meaningRef = useRef(null);
+  const categoryRef = useRef(null);
 
   // console.log("out", category);
 
   const categoryList = ["n", "v", "adj", "adv", "phr", "prep"];
-
+  console.log("qk", contentsRef.current.value);
   const onClickSubmitWords = () => {
-    const formData = new FormData();
-    formData.append("contents", contents);
-    formData.append("spelling", spelling);
-    formData.append("category", category);
-    formData.append("meaning", meaning);
-    console.log(formData);
-    axios
-      .post(
-        "http://52.78.37.13/api/words/",
-        {
-          header: CookieToken,
-        },
-        formData
-      )
-      .then((response) => {
-        console.log(response);
-      });
+    console.log(contentsRef.current.value);
+    // const formData = new FormData();
+    // formData.append("contents", contentsRef.current.value);
+    // formData.append("spelling", spellingRef.current.value);
+    // formData.append("category", categoryRef.current.value);
+    // formData.append("meaning", meaningRef.current.value);
+    // // FormData의 key 확인
+    // for (let key of formData.keys()) {
+    //   console.log(key);
+    // }
+
+    // // FormData의 value 확인
+    // for (let value of formData.values()) {
+    //   console.log(value);
+    // }
+
+    // axios
+    //   .post("http://52.78.37.13/api/words/", formData, {
+    //     headers: {
+    //       Authorization: cookieToken,
+    //     },
+    //   })
+    //   .then((response) => {
+    //     console.log(response);
+    //   });
   };
 
   return (
     <Container>
       <div>
-        <CssTextField
-          value={contents}
-          onChange={onClickContentHandler}
-          label="title"
-          variant="standard"
-        />
+        <CssTextField ref={contentsRef} label="title" variant="standard" />
         <ColorButton
           onClick={() => {
             onClickSubmitWords();
@@ -76,15 +63,15 @@ export default function AddWords() {
       <CardContainer>
         <div>
           <label>단어</label>
-          <Input value={spelling} onChange={onClickSpellingHandler} />
+          <Input ref={spellingRef} />
         </div>
         <div>
           <label>뜻</label>
-          <Input value={meaning} onChange={onClickMeaningHandler} />
+          <Input ref={meaningRef} />
         </div>
         <div>
           <label>품사</label>
-          <NativeSelect value={category} onChange={onClickCategoryHandler}>
+          <NativeSelect ref={categoryRef}>
             {categoryList.map((category, index) => (
               <option key={index} value={category}>
                 {category}
